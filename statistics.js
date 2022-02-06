62648832f37b7cb17272b0995444cfa20efe7f9a
air_studios <76613821+chenglan28@users.noreply.github.com>
2022-02-06 15:35:30 +0800

    缩短了长度

--- statistics.js
+++ statistics.js
@@ -1,43 +1,28 @@
 function statistics(inner){
-    if(typeof inner == "number")inner.toString();
-    else if(typeof inner == "string"){
-        var res = [];
+    if(typeof inner == "number"){
+        inner = inner + '';
         inner = inner.split("");
-        for(var i = 0;i<inner.length;i++){
-            var temp = true;//用于创建新的对象
-            for(var j = 0;j<res.length;j++){
-                if(res[j].text == inner[i]){
-                    res[j].count++;
-                    temp = false;
-                }
-            }
-            if(temp == true){
-                res.push({
-                    "text":inner[i],
-                    "count":1
-                });
+    }
+    else if(typeof inner == "string")inner = inner.split("");
+    else {
+        console.error("Type that cannot be processed");
+        return undefined;
+    }
+    var res = [];
+    for(var i = 0;i<inner.length;i++){
+        var temp = true;//用于创建新的对象
+        for(var j = 0;j<res.length;j++){
+            if(res[j].inner == inner[i]){
+                res[j].count++;
+                temp = false;
             }
         }
-    }
-    else if(typeof inner == "object"){
-        var res = [];
-        inner = inner.split("");
-        for(var i = 0;i<inner.length;i++){
-            var temp = true;//用于创建新的对象
-            for(var j = 0;j<res.length;j++){
-                if(res[j].text == inner[i]){
-                    res[j].count++;
-                    temp = false;
-                }
-            }
-            if(temp == true){
-                res.push({
-                    "text":inner[i],
-                    "count":1
-                });
-            }
+        if(temp == true){
+            res.push({
+                "inner":inner[i],
+                "count":1
+            });
         }
     }
-    else console.error("Type that cannot be processed");
     return res;
 }
