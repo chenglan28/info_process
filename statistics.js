8e190618015a004360daeb7dd9e1349a9f7e187e
air_studios <76613821+chenglan28@users.noreply.github.com>
2022-02-06 15:12:42 +0800

    Update statistics.js

--- statistics.js
+++ statistics.js
@@ -1,19 +1,22 @@
 function statistics(inner){
-    var res = [];
-    inner = inner.split("");
-    for(var i = 0;i<inner.length;i++){
-        var temp = true;
-        for(var j = 0;j<res.length;j++){
-            if(res[j].text == inner[i]){
-                res[j].count++;
-                temp = false;
+    if(typeof inner == "number")inner.toString();
+    else(typeof inner == "string"){
+        var res = [];
+        inner = inner.split("");
+        for(var i = 0;i<inner.length;i++){
+            var temp = true;//用于创建新的对象
+            for(var j = 0;j<res.length;j++){
+                if(res[j].text == inner[i]){
+                    res[j].count++;
+                    temp = false;
+                }
             }
-        }
-        if(temp == true){
-            res.push({
-                "text":inner[i],
-                "count":1
-            });
+            if(temp == true){
+                res.push({
+                    "text":inner[i],
+                    "count":1
+                });
+            }
         }
     }
     return res;
