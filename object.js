b9a1de41e0171bcf7362a8664e60e37238867e15
air_studios <76613821+chenglan28@users.noreply.github.com>
2022-02-06 15:08:44 +0800

    更新显示和隐藏功能
    更新显示和隐藏功能

--- object.js
+++ object.js
@@ -1,16 +1,23 @@
-  function query(object){
-    var ele = document.querySelectorAll(n);
-    //取出所有匹配元素
-    if (ele.length === 1){
-      //只有1个元素
-      //不使用NodeList节点返回
-      var ret = ele[0];
-      ret.forEach = function(callback){
-        //自定义callback函数
-        callback(ret);
-      };
-      ret.length = 1;
-    } else ret = ele;
-    return ret;
-        
-  };
+function query(object){
+  var ele = document.querySelectorAll(object);
+  //取出所有匹配元素
+  var ret;
+  if (ele.length === 1){
+    //只有1个元素
+    //不使用NodeList节点返回
+    ret = ele[0];
+    ret.forEach = function(callback){
+      //自定义callback函数
+      callback(ret);
+    };
+    ret.hidden = function(){
+      ret.display = ret.style.display;
+      ret.style.display = "none";
+    }
+    ret.display = function(){
+      ret.style.display = ret.display;
+    }
+    ret.length = 1;
+  } else ret = ele;
+  return ret;   
+};
