77b09126ed874b2a4faa39c281ab4b337f2ce117
air_studios <76613821+chenglan28@users.noreply.github.com>
2022-02-06 13:24:25 +0800

    Add files via upload

---
+++ object.js
@@ -0,0 +1,4 @@
+function ID(a){return document.getElementById(a)}
+function CLASS(a){return document.getElementsByClassName(a)}
+function TAG(a){return document.getElementsByTagName(a)}
+function NAME(a){return document.getElementsByName(a)}
