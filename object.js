b67543df6268beaa3d628ac1c9c4675c94a609d7
air_studios <76613821+chenglan28@users.noreply.github.com>
2022-02-07 13:44:17 +0800

    Add files via upload
    修复了BUG

--- object.js
+++ object.js
@@ -1,23 +1,25 @@
 function query(object){
-  var ele = document.querySelectorAll(object);
-  //取出所有匹配元素
-  var ret;
-  if (ele.length === 1){
-    //只有1个元素
-    //不使用NodeList节点返回
-    ret = ele[0];
-    ret.forEach = function(callback){
-      //自定义callback函数
-      callback(ret);
-    };
-    ret.length = 1;
-  } else ret = ele;
-  ret.hidden = function(){
-    ret.display = ret.style.display;
-    ret.style.display = "none";
-  }
-  ret.display = function(){
-    ret.style.display = ret.display;
-  }
-  return ret;   
+    var ele = document.querySelectorAll(object);
+    //取出所有匹配元素
+    var ret;
+    if (ele.length === 1){
+        //只有1个元素
+        //不适用NodeList节点返回
+        ret = ele[0];
+        ret.forEach = function(callback){
+            //自定义callback函数
+            callback(ret);
+        };
+        ret.length = 1;
+    } else ret = ele;
+    ret.forEach(function(currentNode){
+        currentNode.hide = function(){
+            currentNode._display = currentNode.style.display;
+            currentNode.style.display = "none";
+        };
+        currentNode.display = function(){
+            currentNode.style.display = currentNode._display;
+        };
+    });
+    return ret;
 };
